528f06a07506ef402575385876ef269d7a9cb8dd
Hiep Doan <hiep.doan@immutable.com>
2025-12-23 12:19:54 +1100

    Update config files

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,12 @@
-export default {
-  experimental: {
-    ppr: true,
-    inlineCss: true,
-    useCache: true
-  },
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.shopify.com',
-        pathname: '/s/files/**'
-      }
-    ]
-  }
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+	/* config options here */
 };
+
+export default nextConfig;
+
+// Enable calling `getCloudflareContext()` in `next dev`.
+// See https://opennext.js.org/cloudflare/bindings#local-access-to-bindings.
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
